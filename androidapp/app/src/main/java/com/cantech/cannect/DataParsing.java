36c15253eae79c679935da56650f0186522910e3
Nicholas Lau <nla52@sfu.ca>
2020-08-17 20:10:03 -0700

    Fixed Hexadecimal conversion operations, changed name field (result[0]) and made DataParsing functions static

--- androidapp/app/src/main/java/com/cantech/cannect/DataParsing.java
+++ androidapp/app/src/main/java/com/cantech/cannect/DataParsing.java
@@ -34,22 +34,31 @@ package com.cantech.cannect;
 
 
 public class DataParsing extends AppCompatActivity{
-    public static final String FUEL_STATUS = "03";
-    public static final String ENGINE_COOLANT_TEMP = "05";
-    public static final String FUEL_PRESSURE = "0A";
-    public static final String ENGINE_RPM = "0C";
-    public static final String VEHICLE_SPEED = "0D";
-    public static final String MAF_SENSOR = "10";
-    public static final String THROTTLE = "11";
-    public static final String O2_VOLTAGE = "14";
+    private static final String FUEL_STATUS = "03";
+    private static final String ENGINE_COOLANT_TEMP = "05";
+    private static final String FUEL_PRESSURE = "0A";
+    private static final String ENGINE_RPM = "0C";
+    private static final String VEHICLE_SPEED = "0D";
+    private static final String MAF_SENSOR = "10";
+    private static final String THROTTLE = "11";
+    private static final String O2_VOLTAGE = "14";
+
+    public static final String FUEL_STATUS_STRING = "FUEL_STATUS";
+    public static final String ENGINE_COOLANT_TEMP_STRING = "ENGINE_COOLANT_TEMP";
+    public static final String FUEL_PRESSURE_STRING = "FUEL_PRESSURE";
+    public static final String ENGINE_RPM_STRING = "ENGINE_RPM";
+    public static final String VEHICLE_SPEED_STRING = "VEHICLE_SPEED";
+    public static final String MAF_SENSOR_STRING = "MAF_SENSOR";
+    public static final String THROTTLE_STRING = "THROTTLE";
+    public static final String O2_VOLTAGE_STRING = "O2_VOLTAGE";
 
     public static final String PID_CURRENT_DATA = "01";
     public static final String PID_FREEZE_DATA = "02";
 
     // OBD2 Stub Function for different Modes
     // Input a set of 8 segments separated by space (eg. "00 00 00 00 00 00 00 00")
-    // Return: User friendly format (PID and actual value)
-    public String[] convertOBD2FrameToUserFormat(String obd2message){
+    // Return: User friendly format (Name of field and actual value)
+    public static String[] convertOBD2FrameToUserFormat(String obd2message){
         String[] splitobd2msg = obd2message.split("\\s+");
         // # bytes: ix=0
         // Mode: ix=1
@@ -70,48 +79,52 @@ public class DataParsing extends AppCompatActivity{
                 result[0] = "UNDEFINED";
                 result[1] = "UNDEFINED";
         }
-
+        System.out.println(result[0]);
+        System.out.println(result[1]);
         return result;
     }
 
     // OBD2 Mode 1 and 2 PID Converter
     // Space separated array of strings containing OBD2 Message
-    // Return: User friendly format (PID and actual value)
-    private String[] convertOBD2PIDToUserFormat(String[] splitobd2msg)
+    // Return: User friendly format (Name of field and actual value)
+    private static String[] convertOBD2PIDToUserFormat(String[] splitobd2msg)
     {
         String[] result = {"",""};
+        float convertA = Float.intBitsToFloat(Integer.parseInt(splitobd2msg[3], 16));
+        float convertB = Float.intBitsToFloat(Integer.parseInt(splitobd2msg[4], 16));
         switch(splitobd2msg[2])
         {
             case FUEL_STATUS:
-                result[0] = FUEL_STATUS;
+                result[0] = FUEL_STATUS_STRING;
                 result[1] = splitobd2msg[3]; // NEED BETTER SYSTEM TO ORGANIZE THIS
                 break;
             case ENGINE_COOLANT_TEMP:
-                result[0] = ENGINE_COOLANT_TEMP;
-                result[1] = Integer.toString(Integer.parseInt(splitobd2msg[3]) - 40);
+                result[0] = ENGINE_COOLANT_TEMP_STRING;
+                result[1] = Integer.toString(Integer.parseInt(splitobd2msg[3], 16) - 40);
                 break;
             case FUEL_PRESSURE:
-                result[0] = FUEL_PRESSURE;
-
+                result[0] = FUEL_PRESSURE_STRING;
+                result[1] = Integer.toString(Integer.parseInt(splitobd2msg[3], 16) * 3);
                 break;
             case ENGINE_RPM:
-                result[0] = ENGINE_RPM;
-                result[1] = Integer.toString((Integer.parseInt(splitobd2msg[3]) * 256) + (Integer.parseInt(splitobd2msg[4]) / 4));
+                result[0] = ENGINE_RPM_STRING;
+                result[1] = Float.toString((convertA * 256) + (convertB / 4));
                 break;
             case VEHICLE_SPEED:
-                result[0] = VEHICLE_SPEED;
-                result[1] = splitobd2msg[3];
+                result[0] = VEHICLE_SPEED_STRING;
+                result[1] = Integer.toString(Integer.parseInt(splitobd2msg[3], 16));
                 break;
             case MAF_SENSOR:
-                result[0] = MAF_SENSOR;
-                result[1] = Integer.toString((Integer.parseInt(splitobd2msg[3]) * 256) + (Integer.parseInt(splitobd2msg[4]) / 100));
+                result[0] = MAF_SENSOR_STRING;
+                result[1] = Float.toString((convertA * 256) + (convertB / 100));
                 break;
             case THROTTLE:
-                result[0] = THROTTLE;
+                result[0] = THROTTLE_STRING;
+                result[1] = Float.toString(convertA * (100.0000f/256.0000f));
                 break;
             case O2_VOLTAGE:
-                result[0] = O2_VOLTAGE;
-                result[1] = Integer.toString((Integer.parseInt(splitobd2msg[3]) * 100) / 255);
+                result[0] = O2_VOLTAGE_STRING;
+                result[1] = Float.toString(convertA / 255);
                 break;
             default:
                 result[0] = "UNDEFINED: " + splitobd2msg[2];
