2f302e4eaabbd017cf19d08527bb81d5872a0f80
Takehiro <tta46@sfu.ca>
2020-08-19 05:30:16 -0700

    Updated the reader code.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,159 +1,164 @@
 /**
-   Arduino Uno & SparkFun CAN Bus shield - Controls the CAN Bus controller
+   ESP32 - Responsible to transmit to Bluetooth devices
 
    References:
-   https://github.com/sparkfun/SparkFun_CAN-Bus_Arduino_Library
+   https://github.com/espressif/arduino-esp32/tree/master/libraries/BluetoothSerial
+   https://github.com/espressif/arduino-esp32/tree/master/libraries/SPI
 */
+
+/**
+   Metadata
+*/
+#define ALPHA_VERSION // uncomment this if you're working on the alpha-phase reader
+#define CURRENT_VERSION "0.0.1"
+
+#ifdef ALPHA_VERSION
+#define ESP32_BL_NAME "ESP32Alpha"
+#else
+#define ESP32_BL_NAME "CANnectReader"
+#endif
 
 /**
    Libraries
 */
-#include <SoftwareSerial.h>
-#include <string.h>
-#include <Canbus.h>
-#include <defaults.h>
-#include <global.h>
-#include <mcp2515.h>
-#include <mcp2515_defs.h>
+#include "BluetoothSerial.h"
+#include <SPI.h>
+#include "mcp_can.h"
 
 /**
-   Arduino Hardware Pin-outs
+   ESP32 Pins
 */
-#define SERIAL_RX             2
-#define SERIAL_TX             3
-
-/**
-   PIDs
-*/
-#define ENGINE_COOLANT_TEMP   0x05
-#define ENGINE_RPM            0x0C
-#define VEHICLE_SPEED         0x0D
-#define MAF_SENSOR            0x10
-#define O2_VOLTAGE            0x14
-#define THROTTLE              0x11
-#define FUEL_PRESSURE         0x0A
-#define FUEL_STATUS           0x03
-
-#define PID_REQUEST           0x7DF
-#define PID_REPLY             0x7E8
+//#define VSPI_MISO    19
+//#define VSPI_MOSI    23
+//#define VSPI_SCLK    18
+#define VSPI_SS      5
+//#define VSPI_INT     21
 
 /**
    Constants
 */
-#define FILTER_FLAG           1 // set this to zero if you want to see everything
-#define MESSAGE_INTERVAL      1000 // 1000 ms
-#define MESSAGE_DELAY         20
-#define BAUD_RATE             115200
+#define MESSAGE_INTERVAL          1000 // 1000 ms
+#define MESSAGE_DELAY             20
+#define BAUD_RATE                 115200
+#define SETUP_DELAY               100
+#define INITIALISATION_ATTEMPTS   5
+#define MESSAGE_END               0xFF
 
 /**
    Global Variables
 */
-SoftwareSerial sw(SERIAL_RX, SERIAL_TX);
-unsigned long loopTime = 0;
+MCP_CAN CAN(VSPI_SS);
+BluetoothSerial SerialBT;
 
 /**
    Setup
 */
 void setup() {
   Serial.begin(BAUD_RATE);
-  Serial.println("Interfacing with the Arduinos");
-  sw.begin(BAUD_RATE);
 
-  if (Canbus.init(CANSPEED_500)) { //Initialise MCP2515 CAN controller at the specified speed
-    Serial.println("CAN Init ok");
-  }
+  SerialBT.begin(ESP32_BL_NAME); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 
-  else {
-    Serial.println("Can't init CAN");
-  }
+#ifndef ALPHA_VERSION
+  setupCANBus();
+#endif
 }
 
 /**
    Loop
 */
 void loop() {
-  unsigned long elapsedTime = millis();
-  unsigned char pids[] = {ENGINE_COOLANT_TEMP, ENGINE_RPM, VEHICLE_SPEED};
-  int pidsLength = sizeof(pids) / sizeof(unsigned char);
-
-  // Send requests every MESSAGE_INTERVAL
-  if (elapsedTime - loopTime >= MESSAGE_INTERVAL) {
-    sw.println("Sending a request");
-    requestPIDs(pids, pidsLength);
-    loopTime = millis();
-  }
-
-  if (sw.available()) {
-    Serial.print(sw.read());
-    delay(MESSAGE_DELAY);
-  }
-
-  checkPIDMessageFilter(pids, pidsLength, false);
-}
-
-void checkPIDMessageFilter(unsigned char* pids, int length, bool filterOn) {
-  tCAN message;
-  if (mcp2515_check_message()) {
-    if (mcp2515_get_message(&message)) {
-      if (filterOn) {
-        for (int ii = 0; ii < length; ii++) {
-          if (message.id == PID_REPLY && message.data[2] == pids[ii]) {
-            printMessage(message);
-            return;
-          }
-        }
-      }
-      else {
-        printMessage(message);
-      }
+#ifdef ALPHA_VERSION
+  if (SerialBT.available()) {
+    while (SerialBT.available()) {
+      Serial.write(SerialBT.read());
+      delay(MESSAGE_DELAY);
     }
+    SerialBT.print(MESSAGE_END);
+    SerialBT.print(MESSAGE_END);
+    Serial.println(" ");
+  }
+
+#else
+  writeCANToBluetooth();
+  receiveFromBluetooth();
+#endif
+  writeSerialToBluetooth();
+}
+
+void writeSerialToBluetooth(void) {
+  if (Serial.available()) {
+    while (Serial.available()) {
+
+      SerialBT.write(Serial.read());
+      delay(MESSAGE_DELAY);
+    }
+    SerialBT.print(MESSAGE_END);
+    SerialBT.print(MESSAGE_END);
+    SerialBT.println(" ");
   }
 }
 
-void printMessage(tCAN message) {
-  Serial.print("ID: ");
-  Serial.print(message.id, HEX);
-  Serial.println(" ");
+void writeCANToBluetooth(void) {
+  unsigned char len = 0;
+  unsigned char buf[64];
 
-  sw.print("ID: ");
-  sw.print(message.id, HEX);
-  sw.print(", ");
-  sw.print("Data: ");
-  sw.print(message.header.length, DEC);
-  for (int i = 0; i < message.header.length; i++) {
-    sw.print(message.data[i], HEX);
-    sw.print(" ");
+  if (CAN_MSGAVAIL == CAN.checkReceive()) {
+    CAN.readMsgBuf(&len, buf);
+
+    unsigned long canId = CAN.getCanId();
+
+    SerialBT.print("Data from ID 0x");
+    SerialBT.println(canId, HEX);
+
+    for (int i = 0; i < len; i++) {
+      SerialBT.print(buf[i]);
+      SerialBT.print("\t");
+    }
+    SerialBT.print(MESSAGE_END);
+    SerialBT.print(MESSAGE_END);
+    SerialBT.println();
   }
-  sw.println(" ");
 }
 
-void requestPID(unsigned char pid) {
-  // Inspired by SparkFun's ECU Demo
+/**
+   CAN Bus Setup
+*/
+void setupCANBus(void) {
+  int attempt = 0;
+  while (CAN_OK != CAN.begin(CAN_500KBPS) && attempt < INITIALISATION_ATTEMPTS) {
+    Serial.println("CAN BUS init attempt Failed");
+    delay(SETUP_DELAY);
+    attempt++;
+  }
 
-  tCAN message;
-  float data;
-  int timeout = 0;
-  char messageACK = 0;
-
-  // Preparing the message
-  message.id = PID_REQUEST;
-  message.header.rtr = 0;
-  message.header.length = 8;
-  message.data[0] = 0x02;
-  message.data[1] = 0x01;
-  message.data[2] = pid;
-  message.data[3] = 0x00;
-  message.data[4] = 0x00;
-  message.data[5] = 0x00;
-  message.data[6] = 0x00;
-  message.data[7] = 0x00;
-
-  mcp2515_bit_modify(CANCTRL, (1 << REQOP2) | (1 << REQOP1) | (1 << REQOP0), 0);
-  mcp2515_send_message(&message);
+  if (attempt < INITIALISATION_ATTEMPTS) {
+    Serial.println("CAN BUS Init Failed");
+  }
+  else {
+    Serial.println("CAN BUS Init OK!");
+  }
 }
 
-void requestPIDs(unsigned char* pids, int length) {
-  for (int ii = 0; ii < length; ii++) {
-    requestPID(pids[ii]);
+void receiveFromBluetooth(void) {
+  if (SerialBT.available()) {
+    Serial.print("Data from Bluetooth device: ");
+
+    long unsigned int buf[64];
+    int ii = 0;
+    while (SerialBT.available()) {
+      Serial.print(convertUint8ToChar(SerialBT.read()));
+      delay(MESSAGE_DELAY);
+    }
+
+    Serial.println(" ");
   }
+}
+
+uint8_t convertCharToUint8(char* string) {
+  return (uint8_t)atoi(string);
+}
+
+char convertUint8ToChar(int number) {
+  return (char)number;
 }
